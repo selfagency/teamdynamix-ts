675553109ae2cc678070390aeaf5a8aaf25df7ad
daniel sieradski <2541728+selfagency@users.noreply.github.com>
2026-06-09 11:07:17 -0400

    fix(docs): remove vitepress-openapi to fix OOM during docs build (#8)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,5 @@
-import { theme, useOpenapi } from 'vitepress-openapi/client';
-import 'vitepress-openapi/dist/style.css';
 import DefaultTheme from 'vitepress/theme';
-import spec from '../../public/openapi.json';
 
 export default {
   extends: DefaultTheme,
-  async enhanceApp({ app }) {
-    useOpenapi({
-      spec,
-    });
-
-    theme.enhanceApp({ app });
-  },
 };
